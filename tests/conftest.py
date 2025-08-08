aa01789d7bedd6c2e1118a233bad65838210b11d
Владимир <52216459+Vladimir1987-web@users.noreply.github.com>
2025-08-08 22:32:08 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,1 @@
-import pytest
 
-from src.product import Product
-
-
-@pytest.fixture
-def products_1():
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
