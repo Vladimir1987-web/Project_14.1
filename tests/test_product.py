1e0a2b377500573ebc981221b097dd71357161d6
Владимир Федотов <vladimir.fedotov1987@yandex.ru>
2025-08-14 06:45:38 +0300

    delete .idea4

--- tests/test_product.py
+++ tests/test_product.py
@@ -2,6 +2,7 @@ import pytest
 
 
 def test_product_init(products_1):
+    """Проверка работы класса"""
     assert products_1.name == "Samsung Galaxy S23 Ultra"
     assert products_1.description == "256GB, Серый цвет, 200MP камера"
     assert products_1.price == 180000.0
