a57e36cf77f72c542b605ba65db74688a3a83054
Владимир Федотов <vladimir.fedotov1987@yandex.ru>
2025-08-14 06:42:37 +0300

    delete .idea4

--- tests/test_product.py
+++ tests/test_product.py
@@ -49,7 +49,7 @@ def test_smartphone_init(smartphone1):
     assert smartphone1.color == "Серый"
 
 
-def test_lawngrass_init(grass1):
+def test_lawngras_init(grass1):
     assert grass1.name == "Газонная трава"
     assert grass1.description == "Элитная трава для газона"
     assert grass1.price == 500.0
